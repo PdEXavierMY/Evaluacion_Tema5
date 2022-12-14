42ad1da69c182cdd6bdf7154f7f03c88a0e043df
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-12-14 17:30:02 +0100

    Update contador.py

--- ejercicios/contador.py
+++ ejercicios/contador.py
@@ -5,3 +5,8 @@ def crear_contadortxt():
     contadortxt = open("contador.txt", "a+") 
     contadortxt.seek(0)
     contenido = contadortxt.readline()
+    if len(contenido) == 0:
+        contenido = "0"
+        contadortxt.write(contenido)
+    contadortxt.close()
+    
