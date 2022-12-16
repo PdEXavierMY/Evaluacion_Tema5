1fb96acc2de4a4f2d7f2ce52b03cb108a208a5bb
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-12-16 15:49:16 +0100

    casi final

--- lanzador.py
+++ lanzador.py
@@ -4,6 +4,7 @@ from ejercicios.calculo import ejercicio
 from ejercicios.reloj import reloj_terminal
 from ejercicios.contador import crear_contadortxt
 from ejercicios.gestor import Gestor, Personaje
+import time
 
 def launcher():
     numero = solicitar_introducir_numero_extremo("Introduzca el ejercicio que desea ejecutar", 1, 4)
@@ -13,20 +14,27 @@ def launcher():
         reloj_terminal()
     elif numero == 3:
         crear_contadortxt()
-        modifica_contador = solicitar_introducir_si_o_no("¿Quieres incrementar o reducir el contador? (Sí/No))")
+        modifica_contador = solicitar_introducir_si_o_no("¿Quieres incrementar o reducir el contador? (Sí/No)): ")
         if modifica_contador:
             numero = solicitar_introducir_numero_extremo("¿Quieres incrementarlo o reducirlo? (Incrementarlo=1, Reducirlo=2)", 1, 2)
             if numero == 1:
-                os.system("C:/Users/javie/AppData/Local/Programs/Python/Python310/python.exe c:/Users/javie/OneDrive/Documents/GitHub/Evaluacion_Tema5/ejercicios/contador.py inc")
+                os.system("")
             elif numero == 2:
-                os.system("C:/Users/javie/AppData/Local/Programs/Python/Python310/python.exe c:/Users/javie/OneDrive/Documents/GitHub/Evaluacion_Tema5/ejercicios/contador.py dec")
+                os.system("")
     elif numero == 4:
         ges = Gestor()
+        print("Añadimos personajes:\n")
+        print("Añadiendo caballero...")
+        time.sleep(1)
         ges.agregar(Personaje("Caballero",4,2,4,2))
+        print("Añadiendo guerrero...")
+        time.sleep(1)
         ges.agregar(Personaje("Guerrero",2,4,2,4))
+        print("Añadiendo arquero...")
+        time.sleep(1)
         ges.agregar(Personaje("Arquero",2,4,1,8))
         ges.mostrar()
-        ges.mostrar()
+        print("Borramos arquero...")
+        time.sleep(1)
         ges.borrar("Arquero")
-        ges.borrar("Caballera")
         ges.mostrar()
